6945bddcfeb7f8274760476bf00912e5aa7c7048
Jennifer GAN <jennifergxl@gmail.com>
2017-08-20 15:09:51 +0800

    set git env

---
+++ git/gitrc.sh
@@ -0,0 +1,4 @@
+export GIT_AUTHOR_NAME="Jennifer GAN"
+export GIT_AUTHOR_EMAIL="jennifergxl@gmail.com"
+export GIT_COMMITTER_NAME="${GIT_AUTHOR_NAME}"
+export GIT_COMMITTER_EMAIL="${GIT_AUTHOR_EMAIL}"
